01815897d3e4dabcf55a2d24b076b6a3af48beae
Mark Baggett <mbagget1@utk.edu>
2016-02-12 15:10:24 -0500

    Make Pep-8 compliant

--- dplachecker.py
+++ dplachecker.py
@@ -9,22 +9,26 @@ parser.add_argument("-s", "--set", dest="oaiset", help="Specify your OAI set", r
 parser.add_argument("-m", "--metadata", dest="metadataprefix", help="Specify your metadata prefix", required=True)
 args = parser.parse_args()
 
+
 def grab_oai(url, token):
     document = etree.parse(url + token)
-    session_token = document.findall('//{http://www.openarchives.org/OAI/2.0/}resumptionToken')
-    publishers = document.xpath('/e:OAI-PMH/e:ListRecords/e:record/e:metadata/f:dc/g:publisher', namespaces={'e': 'http://www.openarchives.org/OAI/2.0/', 'f': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'g': 'http://purl.org/dc/elements/1.1/'})
+    new_session_token = document.findall('//{http://www.openarchives.org/OAI/2.0/}resumptionToken')
+    publishers = document.xpath('/e:OAI-PMH/e:ListRecords/e:record/e:metadata/f:dc/g:publisher',
+                                namespaces={'e': 'http://www.openarchives.org/OAI/2.0/',
+                                            'f': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+                                            'g': 'http://purl.org/dc/elements/1.1/'})
     for publisher in publishers:
         print(publisher.text)
-    if len(session_token) == 1:
-        resumption_token = '&resumptionToken={0}'.format(session_token[0].text)
-        token_attributes = session_token[0].attrib
-        print('Token is at {0} of {1} total records.'.format(token_attributes["cursor"], token_attributes["completeListSize"]))
+    if len(new_session_token) == 1:
+        resumption_token = '&resumptionToken={0}'.format(new_session_token[0].text)
+        token_attributes = new_session_token[0].attrib
+        print('Token is at {0} of {1} total records.'.format(token_attributes["cursor"],
+                                                             token_attributes["completeListSize"]))
         grab_oai(oai_endpoint, resumption_token)
 
 
-
 if __name__ == "__main__":
-    #Defaults
+    # Defaults
     oai_endpoint = 'http://dpla.lib.utk.edu:8080/repox/OAIHandler?verb=ListRecords'
     oai_set = '&set='
     metadata_prefix = '&metadataPrefix='
