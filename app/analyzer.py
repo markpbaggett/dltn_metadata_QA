2a0339095d80641997329177d0e98a865a2df3bf
Mark Baggett <mbagget1@utk.edu>
2020-08-25 11:11:05 -0400

    Add match.

--- app/analyzer.py
+++ app/analyzer.py
@@ -3,9 +3,16 @@ import json
 
 
 class MongoAnalyzer:
-    def __init__(self, field, metadata_format, mongo_collection="default", uri="localhost", port="27017"):
+    def __init__(
+        self,
+        field,
+        metadata_format,
+        mongo_collection="default",
+        uri="localhost",
+        port="27017",
+    ):
         self.formatted_field = self.__format_metadata(field, metadata_format)
-        self.__db_connection = MongoClient(f'mongodb://{uri}:{port}/').dltndata
+        self.__db_connection = MongoClient(f"mongodb://{uri}:{port}/").dltndata
         self.collection = self.__db_connection[mongo_collection]
 
     @staticmethod
@@ -17,14 +24,24 @@ class MongoAnalyzer:
             "oai_qdc": f"metadata.{prefix}:qualifieddc.{field}",
             "oai_etdms": f"metadata.thesis.{field}",
             "digital_commons": f"metadata.document.{field}",
-            "dpla": f"metadata.{field}"
+            "dpla": f"metadata.{field}",
         }
         return formatted_field[prefix]
 
     def find(self):
         return self.collection.distinct(self.formatted_field)
 
+    def match(self, matching_value):
+        return [
+            doc
+            for doc in self.collection.find(
+                json.loads(f'{{ "{self.formatted_field}": "{matching_value}" }}')
+            )
+        ]
+
 
 if __name__ == "__main__":
-    x = MongoAnalyzer("titleInfo", "mods", "all_digital_oai")
-    print(x.find())
+    x = MongoAnalyzer("name.role.roleTerm.#text", "mods", "all_digital_oai")
+    test = x.match("Minute")
+    for document in test:
+        print(document)
